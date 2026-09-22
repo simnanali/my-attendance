642cf5e88bd00ca95577bf765ad424d6e47216c2
simnanali <a.alnaeem@gmail.com>
2026-09-23 00:30:28 +0530

    add allowed origin url

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const attendanceRulesRouter = require('./routes/attendance-rules.routes');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const ALLOWED_ORIGIN = 'http://localhost:4200';
+const ALLOWED_ORIGIN = 'http://localhost:4200' || 'https://my-attendance-u7hz.onrender.com';
 
 app.use(express.json());
 
